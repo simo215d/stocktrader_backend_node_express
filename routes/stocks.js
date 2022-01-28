db5ae5fcd14ca0ee028c6994733065963fe0a953
Simon <simon@irsquadcom>
2022-01-28 12:55:02 +0100

    alpaca api implemented

--- routes/stocks.js
+++ routes/stocks.js
@@ -1,8 +1,21 @@
+var config = require('../config.json');
 var express = require('express');
 var router = express.Router();
+const Alpaca = require('@alpacahq/alpaca-trade-api')
+const alpaca = new Alpaca({
+    keyId: config.ALPACAKEYID,
+    secretKey: config.ALPACASECRETKEY,
+    paper: true,
+})
 
 router.get('/all', function (req, res, next) {
-    res.send('here are stocks: x y z');
+    (async () => {
+        console.log("begun");
+        await alpaca.getAssets({}).then(result => {
+            res.send('result: ' + JSON.stringify(result));
+        });
+        console.log("done");
+    })();
 });
 
 router.get('/buy', function (req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
+var config = require('../config.json');
 var express = require('express');
 var router = express.Router();
+const Alpaca = require('@alpacahq/alpaca-trade-api')
+const alpaca = new Alpaca({
+  keyId: config.ALPACAKEYID,
+  secretKey: config.ALPACASECRETKEY,
+  paper: true,
+})
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET user listing. */
+router.get('/', function (req, res, next) {
+  alpaca.getAccount().then((account) => {
+    res.send('account: ' + JSON.stringify(account));
+  });
 });
 
 module.exports = router;
